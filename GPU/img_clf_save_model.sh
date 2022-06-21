8d1419c4ae79694048917226702a5f98cff8e308
Subin <39542978+subeans@users.noreply.github.com>
2022-06-21 16:54:57 +0900

    shell script for save model

---
+++ GPU/img_clf_save_model.sh
@@ -0,0 +1,5 @@
+python3 img_clf_save_model.py --model resnet50
+python3 img_clf_save_model.py --model inception_v3
+python3 img_clf_save_model.py --model xception
+python3 img_clf_save_model.py --model vgg16
+python3 img_clf_save_model.py --model mobilenet_v2
